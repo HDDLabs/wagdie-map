0f211ebe46489ffe8155e9805379ed9350262f7b
Alex Samarchi <hamdoedevlabs@gmail.com>
2022-07-31 18:03:08 -0500

    chore: updating context

--- components/baseMap.js
+++ components/baseMap.js
@@ -3,7 +3,7 @@ import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import "leaflet-defaulticon-compatibility";
-import AppContext from '../components/appContext'
+import AppContext from '../components/context'
 const Leaflet = require('leaflet');
 
 const BaseMap = ({ mapLocations }) => {
