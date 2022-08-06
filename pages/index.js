3acb90303ec5244c10c7e491c3184ba1f7b50e14
hippolyte42 <hippolytejacque@hotmail.fr>
2022-08-06 21:28:24 +0200

    meta content + prettier

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,30 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { getWikiLocationsData } from '../lib/wiki';
-import Map from '../components/map';
-import MapLegend from '../components/mapLegend';
-import { useContext } from 'react';
-import AppContext from '../components/context'
-
+import Head from "next/head";
+import { useContext } from "react";
+import AppContext from "../components/context";
+import Map from "../components/map";
+import MapLegend from "../components/mapLegend";
+import { getWikiLocationsData } from "../lib/wiki";
+import styles from "../styles/Home.module.css";
 
 export default function Home({ allLocationssData }) {
   const { selectedLocation } = useContext(AppContext);
-  
+
   return (
     <div className={styles.container}>
       <Head>
         <title>World of WAGDIE</title>
-        <meta name="description" content="" />
+        <meta name="description" content="Map of the World of WAGDIE." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <MapLegend 
-          location={selectedLocation}
-        >
-        </MapLegend>
+        <MapLegend location={selectedLocation} />
         <div className={styles.map}>
           <Map mapLocations={allLocationssData} />
         </div>
       </main>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
@@ -36,8 +32,8 @@ export async function getStaticProps() {
 
   return {
     props: {
-      allLocationssData: allLocationssData
+      allLocationssData,
     },
-    revalidate: 1
-  }
+    revalidate: 1,
+  };
 }
