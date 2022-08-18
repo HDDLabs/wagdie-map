96f83db1028f5965e0907e8d7034d5ef3eb4bc9a
Alex Samarchi <hamdoedevlabs@gmail.com>
2022-08-18 15:49:58 -0500

    feat: applied prettier formatting

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { getWikiLocationsData } from '../lib/wiki';
-import Map from '../components/map';
-import MapLegend from '../components/mapLegend';
-import { useContext } from 'react';
-import AppContext from '../components/context'
-
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { getWikiLocationsData } from "../lib/wiki";
+import Map from "../components/map";
+import MapLegend from "../components/mapLegend";
+import { useContext } from "react";
+import AppContext from "../components/context";
 
 export default function Home({ allLocationssData }) {
   const { selectedLocation } = useContext(AppContext);
-  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,16 +18,13 @@ export default function Home({ allLocationssData }) {
       </Head>
 
       <main className={styles.main}>
-        <MapLegend 
-          location={selectedLocation}
-        >
-        </MapLegend>
+        <MapLegend location={selectedLocation}></MapLegend>
         <div className={styles.map}>
           <Map mapLocations={allLocationssData} />
         </div>
       </main>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
@@ -36,8 +32,8 @@ export async function getStaticProps() {
 
   return {
     props: {
-      allLocationssData: allLocationssData
+      allLocationssData: allLocationssData,
     },
-    revalidate: 1
-  }
+    revalidate: 1,
+  };
 }
