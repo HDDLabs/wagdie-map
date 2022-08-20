b64d7b9f21ea6dcbbd7719b5788a2ae415b9e779
HDDLabs <hamdoedevlabs@gmail.com>
2022-08-19 20:10:50 -0500

    feat: styling popups and linking

--- components/map.js
+++ components/map.js
@@ -10,15 +10,51 @@ const BaseMap = () => {
   const imageBounds = [[0, 0], [1000, 1000]]
 
   const mapLocations = [
-    { name: "The Church of Her", coordinates: [520, 467] },
-    { name: "Putrid Forest", coordinates: [545, 540] },
-    { name: "Crow's Den", coordinates: [480, 517] },
-    { name: "Stalwart Fortress", coordinates: [535, 390] },
-    { name: "Ruined Kingdoms of Yore", coordinates: [500, 395] },
-    { name: "The Wound", coordinates: [515, 200] },
-    { name: "Desolate Wastes", coordinates: [520, 280] },
-    { name: "Prisons of Yore", coordinates: [480, 320] },
-    { name: "Grand Elevator", coordinates: [580, 320] },
+    {
+      name: "The Church of Her",
+      coordinates: [520, 467],
+      link: "https://wagdie.wiki/locations/the_church_of_her"
+    },
+    {
+      name: "Putrid Forest",
+      coordinates: [545, 540],
+      link: "https://wagdie.wiki/locations/the_putrid_forest"
+    },
+    {
+      name: "Crow's Den",
+      coordinates: [480, 517],
+      link: "https://wagdie.wiki/locations/the_crows_den"
+    },
+    {
+      name: "Stalwart Fortress",
+      coordinates: [535, 390],
+      link: "https://wagdie.wiki/locations/stalwart_fortress"
+    },
+    {
+      name: "Ruined Kingdoms of Yore",
+      coordinates: [500, 395],
+      link: "https://wagdie.wiki/locations/the_kingdoms_of_yore"
+    },
+    {
+      name: "The Wound",
+      coordinates: [515, 200],
+      link: "https://wagdie.wiki/locations/the_wound"
+    },
+    {
+      name: "Desolate Wastes",
+      coordinates: [520, 280],
+      link: "https://wagdie.wiki/locations/desolate_wastes"
+    },
+    {
+      name: "Prisons of Yore",
+      coordinates: [480, 320],
+      link: "https://wagdie.wiki/locations/prisons_of_yore"
+    },
+    {
+      name: "Grand Elevator",
+      coordinates: [580, 320],
+      link: "https://wagdie.wiki/locations/the_grand_elevator"
+    },
   ];
 
   const ourLocationIcon = Leaflet.icon({
@@ -47,20 +83,24 @@ const BaseMap = () => {
         bounds={imageBounds}
       />
 
-      <Marker position={[524, 450]} icon={ourLocationIcon}></Marker>
-      {mapLocations.map(({ name, coordinates }) => (
+      <Marker position={[540, 400]} icon={ourLocationIcon}></Marker>
+      {mapLocations.map(({ name, coordinates, link }) => (
         <Marker
           key={name}
           position={coordinates}
           icon={locationIcon}
         >
           <Popup>
-            {name}
+            <a
+              href={link}>
+              {name}
+            </a>
           </Popup>
         </Marker>
-      ))}
+      ))
+      }
 
-    </MapContainer>
+    </MapContainer >
   )
 }
 
