0cae4473f12ba0ab237e7c0c32eb2281fcbf16b1
HDDLabs <hamdoedevlabs@gmail.com>
2022-08-19 20:20:07 -0500

    feat: updating title to World of WAGDIE

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Map of WAGDIE</title>
+        <title>World of WAGDIE</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
