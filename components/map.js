bb0bbdce8fdcf8ccb8a04807758618510d8a8863
HDDLabs <hamdoedevlabs@gmail.com>
2022-08-19 20:10:49 -0500

    feat: adding all map markers correctly

--- components/map.js
+++ components/map.js
@@ -1,17 +1,29 @@
 import React, { useState, useRef, useEffect } from "react";
-import { MapContainer, ImageOverlay, SVGOverlay, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import "leaflet-defaulticon-compatibility";
 const Leaflet = require('leaflet');
 
 const Map = () => {
-  const center = [500, 500]
-  const bounds = [[0, 0], [1000, 1000]]
+  const mapCenter = [500, 500]
+  const imageBounds = [[0, 0], [1000, 1000]]
+
+  const mapLocations = [
+    { name: "The Church of Her", coordinates: [520, 467] },
+    { name: "Putrid Forest", coordinates: [545, 540] },
+    { name: "Crow's Den", coordinates: [480, 517] },
+    { name: "Stalwart Fortress", coordinates: [535, 390] },
+    { name: "Ruined Kingdoms of Yore", coordinates: [500, 395] },
+    { name: "The Wound", coordinates: [515, 200] },
+    { name: "Desolate Wastes", coordinates: [520, 280] },
+    { name: "Prisons of Yore", coordinates: [480, 320] },
+    { name: "Grand Elevator", coordinates: [580, 320] },
+  ];
 
   return (
     <MapContainer
-      center={center}
+      center={mapCenter}
       zoom={2}
       maxZoom={6}
       minZoom={0}
@@ -21,18 +33,20 @@ const Map = () => {
 
       <ImageOverlay
         url="../images/wagdiemap.jpeg"
-        bounds={bounds}
+        bounds={imageBounds}
       />
 
-      <Marker
-        position={[40.8054, -74.0241]}
-        draggable={true}
-        animate={true}
-      >
-        <Popup>
-          Hey ! you found me
-        </Popup>
-      </Marker>
+      {mapLocations.map(({ name, coordinates }) => (
+        <Marker
+          key={name}
+          position={coordinates}
+        >
+          <Popup>
+            {name}
+          </Popup>
+        </Marker>
+      ))}
+
     </MapContainer>
   )
 }
