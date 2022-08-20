5bf32f9e8b45a818e7f5b2a22c20f589b1886abd
HDDLabs <hamdoedevlabs@gmail.com>
2022-08-19 20:10:50 -0500

    feat: updating title

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>WAGDIE Map</title>
+        <title>Map of WAGDIE</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
