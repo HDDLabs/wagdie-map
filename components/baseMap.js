2f816b7596a6eaac060f858ae7251fe9bc8e3af3
HDDLabs <hamdoedevlabs@gmail.com>
2022-08-31 18:36:36 -0500

    feat: decreasing minZoom level

--- components/baseMap.js
+++ components/baseMap.js
@@ -96,7 +96,7 @@ const BaseMap = ({ mapData }) => {
       center={mapCenter}
       zoom={2}
       maxZoom={3}
-      minZoom={1}
+      minZoom={0}
       scrollWheelZoom={true}
       style={{ height: "100%", width: "100%" }}
       crs={Leaflet.CRS.Simple}
