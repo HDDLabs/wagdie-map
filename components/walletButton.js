897dacfdec9f771289af7d046fb5f3bbdacf7aed
HDDLabs <hamdoedevlabs@gmail.com>
2022-11-21 14:56:53 -0600

    fix: fixing the connect button state

--- components/walletButton.js
+++ components/walletButton.js
@@ -1,9 +1,11 @@
 import { useAccount, useConnect, useDisconnect, useEnsName } from "wagmi";
+import { useEffect, useState } from "react";
 
 import { InjectedConnector } from "wagmi/connectors/injected";
 import styles from "../styles/walletButton.module.css";
 
 export const WalletButton = () => {
+  const [_isConnected, _setIsConnected] = useState(false);
   const { address, isConnected } = useAccount();
   const { data: ensName } = useEnsName({ address });
   const { disconnect } = useDisconnect();
@@ -11,12 +13,16 @@ export const WalletButton = () => {
     connector: new InjectedConnector(),
   });
 
+  useEffect(() => {
+    _setIsConnected(isConnected);
+  }, [isConnected]);
+
   return (
     <div
       className={styles.walletButtonContainer}
-      onClick={() => (isConnected ? disconnect() : connect())}
+      onClick={() => (_isConnected ? disconnect() : connect())}
     >
-      {isConnected ? (
+      {_isConnected ? (
         <img src={"/images/walletbutton/button-disconnect.png"} />
       ) : (
         <img src={"/images/walletbutton/button-connect.png"} />
